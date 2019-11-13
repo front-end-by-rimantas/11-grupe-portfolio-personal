b0fa82a8662eb36012c4af871045ab1d72d5119f
makeMHDB <57128981+makeMHDB@users.noreply.github.com>
2019-11-13 11:14:24 +0200

    Brand structure

--- js/data.js
+++ js/data.js
@@ -20,7 +20,30 @@
 
 // testimonials
 
-// contact me
+// brands
+
+const brands = [
+    {
+        name: 'l1',
+        source: './img/l1.png'
+    },
+    {
+        name: 'l2',
+        source: './img/l2.png'
+    },
+    {
+        name: 'l3',
+        source: './img/l3.png'
+    },
+    {
+        name: 'l4',
+        source: './img/l4.png'
+    },
+    {
+        name: 'l5',
+        source: './img/l5.png'
+    },
+]
 
 // footer
 
--- js/functions.js
+++ js/functions.js
@@ -20,7 +20,17 @@
 
 // testimonials
 
-// contact me
+// brands
+
+function renderBrands ( data ) {
+    let HTML = '';
+
+    for( let i=0; i<data.length; i++) {
+        HTML += `<a href="#"><img src="${data[i].source}" alt="${data[i].name}"></a>`
+    }
+
+    return document.querySelector(".row.brands").innerHTML = HTML;
+}
 
 // footer
 
