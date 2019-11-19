0d93d5bea4792f5aad782db1fd38920713de03b1
jarosasan <jaroslavlo@gmail.com>
2019-11-19 12:50:39 +0200

    Gallery rendering and project filtering

--- js/actions.js
+++ js/actions.js
@@ -16,13 +16,31 @@ renderFirstLevelMenu(menu);
 
 // job history
 
-// services
+// projects
+
+renderProjectFilters(categories)
+renderGallery(projects)
+let activeFilter = document.querySelector('.gallery .filters li.filter-active')
+var category = activeFilter.getAttribute('filter-data')
+const ul = document.querySelector('.filters')
+ul.addEventListener('click', (e) => {
+    activeFilter = document.querySelector('.gallery .filters li.filter-active')
+    let target = e.target
+    activeFilter.classList.remove('filter-active')
+    target.classList.add('filter-active')
+    category = target.getAttribute('filter-data')
+    let data = getGalleryData(projects, category)
+    renderGallery(data)
+
+})
+
+
 
 // testimonials
 
 // brands
 
-renderBrands (brands);
+renderBrands(brands);
 
 // footer
 
