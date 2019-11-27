f3c93e200498e455fc738830a4d8cf434c7c513a
jarosasan <jaroslavlo@gmail.com>
2019-11-27 10:31:33 +0200

    Add class active to mobile an desktop menus.

--- js/actions.js
+++ js/actions.js
@@ -6,15 +6,16 @@ renderMobileMenu(menu);
 const hamburger = document.querySelector('.hamburger')
 const overlay = document.querySelector('.overlay')
 const mContainer = document.querySelector('.m-container')
-let nera = true
-let neraPosition = 0
+const allMenuItems = document.querySelectorAll('.menu-item > a')
+let scroll = true
+let scrollPosition = 0
 hamburger.addEventListener('click', () => {
     const child = hamburger.childNodes[0]
     child.classList.toggle('lnr-menu')
     child.classList.toggle('lnr-cross')
     mContainer.style.display = mContainer.style.display === 'grid' ? 'none' : 'grid'
-    nera = false
-    neraPosition = window.scrollY
+    scroll = false
+    scrollPosition = window.scrollY
 })
 
 overlay.addEventListener('click', () => {
@@ -22,28 +23,72 @@ overlay.addEventListener('click', () => {
     child.classList.toggle('lnr-menu')
     child.classList.toggle('lnr-cross')
     mContainer.style.display = mContainer.style.display === 'none' ? 'grid' : 'none'
-    nera = true
+    scroll = true
 })
 
+const mobileMenuItems = document.querySelectorAll('.m-menu .menu-item > a')
+const desktopMenuItems = document.querySelectorAll('.top-menu .menu-item > a')
 
-window.addEventListener('scroll', (e) => {
-    if (nera === false) {
+desktopMenuItems.forEach((i) => {
+    i.addEventListener('click', (e) => {
+        let activeElements = document.querySelectorAll('.menu-item-active')
+        scroll = true
+        activeElements.forEach((el) => {
+            el.classList.remove('menu-item-active')
+        })
+        let item = e.target
+        let itemText = item.textContent
+
+        allMenuItems.forEach((i) => {
+            if (i.textContent === itemText) {
+                i.classList.add('menu-item-active')
+            }
+        })
+    })
+})
+
+mobileMenuItems.forEach((i) => {
+    i.addEventListener('click', (e) => {
+        let activeElements = document.querySelectorAll('.menu-item-active')
+        scroll = true
+        mContainer.style.display = mContainer.style.display === 'none' ? 'grid' : 'none'
+        const child = hamburger.childNodes[0]
+        child.classList.toggle('lnr-menu')
+        child.classList.toggle('lnr-cross')
+        activeElements.forEach((el) => {
+            el.classList.remove('menu-item-active')
+        })
+
+        let item = e.target
+        let itemText = item.textContent
+
+        allMenuItems.forEach((i) => {
+            if (i.textContent === itemText) {
+                i.classList.add('menu-item-active')
+            }
+        })
+    })
+})
+
+window.addEventListener('scroll', () => {
+    if (scroll === false) {
         window.scrollTo({
-            top: neraPosition,
+            top: scrollPosition,
             left: 0,
             behavior: 'smooth'
         })
     }
 })
 
-
 document.addEventListener('click', (e) => {
-    let item = e.target
-    console.log(item)
-    let a = document.querySelector('.menu-item-active').classList.remove('menu-item-active')
-    console.log(a)
-    item.classList.add('menu-item-active')
-    console.log(document.querySelector('.menu-item-active'))
+    // let item = e.target
+    // console.log(item.text)
+    // console.log(item.tagName)
+    // console.log(item.tagName)
+    // console.log(item.classList)
+    // let a = document.querySelector('.menu-item-active').classList.remove('menu-item-active')
+    // item.classList.add('menu-item-active')
+    // console.log(document.querySelector('.menu-item-active'))
 })
 
 
